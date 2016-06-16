34c6b6ce9758f170ad6220cf96924692db20f66d
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-06-16 21:46:37 +0200

    Changes the JS

--- static/app.js
+++ static/app.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   function updatePicture() {
     var percentage = convertPercentage(window.scrollY) / (parallaxRect.top - 400);
-    parallaxBackground.style.backgroundPosition = "0% " + percentage * 40 + "%";
+    parallaxBackground.style.backgroundPosition = "200vw " + percentage * 40 + "%";
   }
 
   function convertPercentage(percentage) {
