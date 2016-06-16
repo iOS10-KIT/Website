c266a3c05755c40d96f5467827aa1dcbd585154a
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-06-16 20:04:37 +0200

    Adds a bit more of javascript

--- static/app.js
+++ static/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   var parallaxBackground = document.getElementById('parallax-wrapper');
+  var mouseEffectImage = document.getElementById('hero-header');
   var parallaxRect = parallaxBackground.getBoundingClientRect();
 
   window.requestAnimationFrame = window.requestAnimationFrame
@@ -17,6 +18,21 @@ document.addEventListener("DOMContentLoaded", function() {
     } else {
       parallaxBackground.style.backgroundPosition = "0% 20%";
     }
+  }
+
+  window.onmousemove = function() {
+    var event = window.event;
+    var maximumVertical = window.innerHeight * 0.9 / 2;
+    var maximumHorizontal = window.innerWidth / 2;
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
+
+    if (maximumVertical - 100 >= mouseY && maximumVertical + 100 <= mouseY
+    && maximumHorizontal - 100 >= mouseX && maximumHorizontal + 100 <= mouseX) {
+      console.log('Center');
+    }
+
+    mouseEffectImage.style.backgroundPosition = "50% 100%";
   }
 
   function updatePicture() {
